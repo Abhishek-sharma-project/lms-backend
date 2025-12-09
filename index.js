f5647f60a7e55a1b0a231ff6d78bf6d100a462ed
Abhishek Sharma <abhisheksharmaword@gmail.com>
2025-12-09 20:22:30 +0530

    Update cors to use frontend url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
